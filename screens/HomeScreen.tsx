24b788941dd1369dc9832bd0e6ebed906ae2a369
David256 <wasterdavid@gmail.com>
2022-06-04 05:40:40 -0500

    Use the API in the Home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,14 +3,24 @@ import { StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
 import SensorStats from '../components/SensorStats';
 
+import useAPI from '../hooks/useAPI';
+
 export default function HomeScreen() {
+  const api = useAPI();
+
+  const distance = api.getApiValue('distance');
+  const co = api.getApiValue('co');
+  const power = api.getApiValue('power');
+  const light = api.getApiValue('light');
+  const humidity = api.getApiValue('humidity');
+
   return (
     <View style={styles.container}>
-      <SensorStats title="Monóxido de carbono:" percentage={0.1}/>
-      <SensorStats title="Humedad:" percentage={0.9}/>
-      <SensorStats title="Intensidad luminosa::" percentage={0.9}/>
-      <SensorStats title="Potenciometro::" percentage={0.9}/>
-      <SensorStats title="Distancia:" percentage={0.9}/>
+      <SensorStats title="Monóxido de carbono:" percentage={Number.parseFloat(co as string)}/>
+      <SensorStats title="Humedad:" percentage={Number.parseFloat(humidity as string)}/>
+      <SensorStats title="Intensidad luminosa::" percentage={Number.parseFloat(light as string)}/>
+      <SensorStats title="Potenciometro::" percentage={Number.parseFloat(power as string)}/>
+      <SensorStats title="Distancia:" percentage={Number.parseFloat(distance as string)}/>
     </View>
   );
 }
