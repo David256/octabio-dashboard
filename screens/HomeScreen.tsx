073a7860536532c4970c7f17bddeed3500c069ae
David256 <wasterdavid@gmail.com>
2022-06-04 07:37:50 -0500

    Add a floating button in Home

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,33 @@
 import { StyleSheet } from 'react-native';
 
+import { FloatingAction, IActionProps } from 'react-native-floating-action';
+
 import { View } from '../components/Themed';
+
+import { colorPrimary } from '../constants/Colors';
+
 import SensorStats from '../components/SensorStats';
 
 import useAPI from '../hooks/useAPI';
+import { FontAwesome } from '@expo/vector-icons';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types';
 
-export default function HomeScreen() {
+const actions: IActionProps[] = [
+  {
+    name: 'control',
+    text: 'Control',
+    icon: <FontAwesome name='gamepad' style={{ marginBottom: 4 }} size={24} color={'#fff'}/>
+  }
+];
+
+export interface HomeScreenProps {
+  navigation: NativeStackNavigationProp<RootStackParamList>,
+};
+
+export default function HomeScreen(props: HomeScreenProps) {
+  const { navigation } = props;
+
   const api = useAPI();
 
   const distance = api.getApiValue('distance');
@@ -14,6 +36,12 @@ export default function HomeScreen() {
   const light = api.getApiValue('light');
   const humidity = api.getApiValue('humidity');
 
+  const onFloatingPressItem = (name?: string | undefined) => {
+    if (name === 'control') {
+      navigation.navigate('Settings')
+    }
+  };
+
   return (
     <View style={styles.container}>
       <SensorStats title="Monóxido de carbono:" percentage={Number.parseFloat(co as string)}/>
@@ -21,6 +49,13 @@ export default function HomeScreen() {
       <SensorStats title="Intensidad luminosa::" percentage={Number.parseFloat(light as string)}/>
       <SensorStats title="Potenciometro::" percentage={Number.parseFloat(power as string)}/>
       <SensorStats title="Distancia:" percentage={Number.parseFloat(distance as string)}/>
+
+      <FloatingAction
+        actions={actions}
+        color={colorPrimary}
+        onPressItem={onFloatingPressItem}
+        floatingIcon={<FontAwesome name='bars' style={{ marginBottom: 4 }} size={24} color={'#fff'}/>}
+      />
     </View>
   );
 }
