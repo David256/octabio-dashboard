0c85ba7326b0086c3153e8ff7afd6491edaf0f3b
David256 <wasterdavid@gmail.com>
2022-06-04 10:40:22 -0500

    Update ControlScreen

--- screens/ControlScreen.tsx
+++ screens/ControlScreen.tsx
@@ -5,6 +5,10 @@ import { Platform, StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import Separator from '../components/Separator';
 import { RootStackParamList } from '../types';
+import { Switch } from '../components/Switch';
+import { MotorSetter } from '../components/MotorSetter';
+
+import { useState } from 'react';
 
 export interface ControlScreenProps {
   navigation: NativeStackNavigationProp<RootStackParamList>,
@@ -13,10 +17,20 @@ export interface ControlScreenProps {
 export default function ControlScreen(props: ControlScreenProps) {
   const { navigation } = props;
 
+  const [motor1, setMotor1] = useState(0);
+  const [motor2, setMotor2] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Control</Text>
       <Separator />
+
+      <Switch left='manual' right='automático' />
+
+      <View style={styles.motorsetters}>
+        <MotorSetter name='motor 1' value={motor1} setValue={setMotor1} />
+        <MotorSetter name='motor 2' value={motor2} setValue={setMotor2} />
+      </View>
   
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -34,5 +48,11 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  motorsetters: {
+    marginTop: 10,
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
